933047b3774953ebe6203df943437e29ac306779
Arsham Roshannejad <aqaarsham@gmail.com>
2023-06-14 15:28:08 +0330

    Add mysql db, Config db connection with gorm, Using logger for app info

---
+++ configs/database.go
@@ -0,0 +1,23 @@
+package configs
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func GetDB() (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"), os.Getenv("DB_PASS"),
+		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err.Error())
+	}
+	return db, nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+
+	"github.com/iarsham/url-shortener/configs"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		panic("environment variables file error!")
+	}
+	_, err := configs.GetDB()
+	if err == nil {
+		configs.Logger.Println("Mysql is Connected Successfuly!")
+	}
+}
 
 func main() {
+	r := gin.Default()
+	r.Use(gin.Recovery())
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"response": "Hello World"})
+	})
+	r.Run(":8000")
 }
